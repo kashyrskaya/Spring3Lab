338c4cf8db7cd8e86aeff742ca564845d263a4f1
anastasiakasirskaa <anastasia.kashyrskaya@gmail.com>
2026-06-11 18:13:59 +0200

    Initial Commit

--- src/main/java/lt/esdc/config/SecurityConfig.java
+++ src/main/java/lt/esdc/config/SecurityConfig.java
@@ -47,6 +47,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/info").permitAll()
                         .requestMatchers(HttpMethod.GET, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
